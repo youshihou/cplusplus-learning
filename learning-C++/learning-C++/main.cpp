d62b88e46a516e38cd3f775241e8003eeb86c83b
oneday <youshihou5@gmail.com>
2020-06-12 20:13:29 +0800

    Initial commit

---
+++ learning-C++/learning-C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learning-C++
+//
+//  Created by Ankui on 6/12/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
