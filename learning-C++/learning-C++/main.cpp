3197bee623ec331ca1347c214eeb7be99e0d6840
oneday <youshihou5@gmail.com>
2020-06-12 23:50:11 +0800

    function overload and default parameter

--- learning-C++/learning-C++/main.cpp
+++ learning-C++/learning-C++/main.cpp
@@ -8,8 +8,81 @@
 
 #include <iostream>
 
+using namespace std;
+
+
+/*     1.function overload          */
+int sum(int a, int b) {
+    return a + b;
+}
+
+int sum(int a, int b, int c) {
+    return a + b + c;
+}
+
+void print(int a, double b) {
+    cout << __func__ << endl;
+}
+
+void print(double a, int b) {
+    cout << __func__ << endl;
+}
+
+
+/*        2.default parameter          */
+int func(int v1 = 5, int v2 = 6) {
+    return v1 + v2;
+}
+
+int func2(int v1, int v2 = 10);
+
+int g_age = 10;
+int func3(int v1, int v2 = g_age);
+
+void test(int a) {
+    cout << a << endl;
+}
+void func4(int v1, void(*p)(int) = test) {
+    p(v1);
+}
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    {
+#if 0
+    cout << sum(10, 20) << endl;
+    cout << sum(10, 20, 30) << endl;
+    
+    print(10, 20.0);
+    print(10.0, 20);
+#endif
+    }
+    
+    {
+#if 1
+        func();
+        func(10);
+        func(10, 20);
+        
+        
+        void(*func)(int) = test;
+        func(10);
+        
+        func4(100, test);
+        func4(120);
+#endif
+    }
+    
+    
+//    getchar();
     return 0;
+}
+
+int func2(int v1, int v2) {
+    return v1 + v2;
+}
+
+int func3(int v1, int v2) {
+    return v1 + v2;
 }
