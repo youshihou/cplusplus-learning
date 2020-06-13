ba7f0e8cf51dfb0f63c38ba284537d1458f90b66
oneday <youshihou5@gmail.com>
2020-06-13 22:34:23 +0800

    struct and class

--- learning-C++/learning-C++/main.cpp
+++ learning-C++/learning-C++/main.cpp
@@ -10,9 +10,59 @@
 
 using namespace std;
 
+class Person_ {
+//private: // class default private
+    int m_age;
+    
+public:
+    void run() {
+        cout << __func__ << ", age = " << m_age << endl;
+    }
+};
+
+struct Person {
+//private: // struct default public
+    int m_id;
+    int m_age;
+    int m_height;
+    
+    void run() {
+        cout << __func__ << ", age = " << m_age << endl;
+//        this->m_age; // this is a pointer
+//        this.m_age; // ERROR!!! . left must be object
+    }
+    
+    void display() {
+        cout << "id = " << m_id << ", age = " << m_age << ", height = " << m_height << endl;
+    }
+};
+
+
 
 int main(int argc, const char * argv[]) {
-
+//    Person_ pp;
+//    pp.m_age = 20; // CARE!!!
+//    pp.run();
+//
+//    Person p;
+//    p.m_age = 10;
+//    p.run();
+//    Person *p1 = &p;
+//    p1->m_age = 20;
+    
+    
+    Person p;
+    p.m_id = 10;
+    p.m_age = 20;
+    p.m_height = 30;
+    
+    Person *pp = (Person *)&p.m_age;
+    pp->m_id = 40;
+    pp->m_age = 50;
+    
+    p.display(); // CARE!!! this == p, is &p
+    pp->display(); // CARE!!! this == pp, is &p.m_age
+    
     return 0;
 }
 
