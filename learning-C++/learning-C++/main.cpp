cc18f365346b660f5849aede9ba2549e46eadda2
oneday <youshihou5@gmail.com>
2020-06-13 19:43:04 +0800

    const variable

--- learning-C++/learning-C++/main.cpp
+++ learning-C++/learning-C++/main.cpp
@@ -66,12 +66,30 @@ struct Date {
 };
 
 
+/*          5.reference          */
+void swap1(int *v1, int *v2) {
+    int tmp = *v1;
+    *v1 = *v2;
+    *v2 = tmp;
+}
 
+void swap2(int &v1, int &v2) {
+    int tmp = v1;
+    v1 = v2;
+    v2 = tmp;
+}
+
+int sum_(int &v1, int &v2) {
+    return v1 + v2;
+}
+
+int sum_(const int &v1, const int &v2) { // const parameter, reference function overload
+    return v1 + v2;
+}
 
 
 
 int main(int argc, const char * argv[]) {
-
     {
 #if 0
     cout << sum(10, 20) << endl;
@@ -81,7 +99,6 @@ int main(int argc, const char * argv[]) {
     print(10.0, 20);
 #endif
     }
-    
     {
 #if 0
         func();
@@ -95,7 +112,6 @@ int main(int argc, const char * argv[]) {
         func4(120);
 #endif
     }
-    
     {
 #if 0
 //        const Date d = {2020, 06, 13};
@@ -176,13 +192,122 @@ int main(int argc, const char * argv[]) {
         
 #endif
     }
+    {
+#if 1
+//        int height = 20;
+        int age = 10;
+        int &ref = age;
+//        ref = 20;
+//        ref += 30;
+//        cout << age << endl;
+        
+//        ref = height;
+//        ref = 11;
+//        cout << height << endl;
+        
+        int &ref1 = ref;
+        int &ref2 = ref1;
+        ref += 10;
+        ref1 += 10;
+        ref2 += 10;
+//        cout << age << endl;
+        
+        int a = 10;
+        int b = 20;
+//        swap1(&a, &b);
+        swap2(a, b);
+//        cout << "a = " << a << ", b = " << b << endl;
+        
+        int weight = 10;
+        int *p = &weight;
+        int * &ref_ = p;
+        *ref_ = 30;
+        
+        int number = 40;
+        ref_ = &number;
+        
+        int array[] = {1, 2, 3};
+        int (&ref__)[3] = array;
+        ref__[0] = 10;
+        int * const &ref2__ = array; // same
+        ref2__[0] = 20;
+        
+        
+//        int *arr1[3] = {p, p, p}; // pointer array, save 3 int *
+//        int (*arr2)[3]; // array pointer, save 3 int
+        
+//        int &arr3[3]; // ERROR!!!, not exist
+        
+        
+        const int *pp = &age;
+//        *pp = 20;
+        cout << *pp << endl;
+        
+        int * const p_ = &age; // p can not modify, *p can modify
+//        p_ = &weight; // ERROE!!!
+        *p_ = 20;
+        int const *p__ = &age; // p can modify, but *p can not modify
+        p__ = &weight;
+//        *p__ = 30; // ERROR!!!
+
+        
+        const int &ref___ = age; // can access, but can not modify
+//        ref___ = 30;
+        int const &ref1___ = age; // same
+        cout << ref___ << endl << ref1___ << endl;
+        
+        
+//        int const & const ref7_ = age; // not allowed
+//        ref7_ = 20;
+//        int & const ref6_ = age; // not allowed, meaningless
+        int &ref6__ = age;
+        ref6__ = 30;
+        
+        
+        age = 10;
+//        int &ref66 = 30; // ERROR!!!
+//        int &ref66_ = a + b; // ERROE!!!
+//        int &ref66__ = sum(1, 2); // ERROR!!!
+//        double &ref66 = age; // ERROR!!!
+        int &ref66 = age; // must be a variable
+        ref66 = 20;
+    
+//        const int &ref666 = 30; // allowed
+//        const int &ref666_ = a + b; // allowed
+//        const int &ref666__ = sum(1, 2); // allowed
+//        const double &ref666___ = age; // allowed
+        
+        
+        a = 10;
+        b = 20;
+        sum_(a, b);
+        sum_(10, 20); // CARE!!!
+        const int aa = 10;
+        const int bb = 20;
+        sum_(aa, bb);
+        
+        
+        age = 10;
+        const int &rAge = age;
+        age = 20;
+        cout << "age = " << age << endl;
+        cout << "rAge = " << rAge << endl;
+        
+        age = 10;
+        const double &rAge_ = age;
+        age = 30;
+        cout << "age = " << age << endl;
+        cout << "rAge_ = " << rAge_ << endl;
+
+        
+#endif
+    }
     
     
     
     
     
-    
-    getchar();
+//    getchar();
     return 0;
 }
 
