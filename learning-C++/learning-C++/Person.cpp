5da0f89ec588190c7abc34a1a4cccabd4623e935
oneday <youshihou5@gmail.com>
2020-06-14 12:11:05 +0800

    hpp and cpp

---
+++ learning-C++/learning-C++/Person.cpp
@@ -0,0 +1,9 @@
+//
+//  Person.cpp
+//  learning-C++
+//
+//  Created by Ankui on 6/14/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "Person.hpp"
---
+++ learning-C++/learning-C++/Person.hpp
@@ -0,0 +1,14 @@
+//
+//  Person.hpp
+//  learning-C++
+//
+//  Created by Ankui on 6/14/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef Person_hpp
+#define Person_hpp
+
+#include <stdio.h>
+
+#endif /* Person_hpp */
