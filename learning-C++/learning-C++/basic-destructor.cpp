0238bd1038b7f79db8f3f486a887f3350695f2ad
oneday <youshihou5@gmail.com>
2020-06-14 10:54:17 +0800

    destructor

--- learning-C++/learning-C++/basic-destructor.cpp
+++ learning-C++/learning-C++/basic-destructor.cpp
@@ -7,3 +7,57 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+#if 0
+struct Car {
+    int m_price;
+    
+    Car () {
+        m_price = 0;
+        cout << __func__ << endl;
+    }
+    
+    ~Car () {
+        cout << __func__ << endl;
+    }
+};
+
+struct Person {
+    int m_age;
+    Car *m_car;
+    
+    Person () {
+        m_age = 0;
+        m_car = new Car();
+        cout << __func__ << endl;
+    }
+    
+    ~Person () {
+        delete m_car;
+        cout << __func__ << endl;
+    }
+};
+#endif
+
+
+//int main(int argc, const char * argv[]) {
+int basic_destructor() {
+    {
+#if 0
+        Person p; // call destructor
+        Person *p0 = new Person; // call destructor
+        delete p0;
+        Person *p1 = (Person *)malloc(sizeof(Person)); // not call destructor
+        free(p1);
+#endif
+        
+    }
+    
+//    Person p;
+    
+    
+    return 0;
+}
--- learning-C++/learning-C++/main.cpp
+++ learning-C++/learning-C++/main.cpp
@@ -10,49 +10,21 @@
 
 using namespace std;
 
-struct Car {
-    int m_price;
-    
-    Car () {
-        m_price = 0;
-        cout << __func__ << endl;
-    }
-    
-    ~Car () {
-        cout << __func__ << endl;
-    }
-};
-
 struct Person {
     int m_age;
-    Car *m_car;
     
     Person () {
         m_age = 0;
-        m_car = new Car();
         cout << __func__ << endl;
     }
     
     ~Person () {
-        delete m_car;
         cout << __func__ << endl;
     }
 };
 
 
 int main(int argc, const char * argv[]) {
-    {
-#if 0
-        Person p; // call destructor
-        Person *p0 = new Person; // call destructor
-        delete p0;
-        Person *p1 = (Person *)malloc(sizeof(Person)); // not call destructor
-        free(p1);
-#endif
-        
-    }
-    
-    Person p;
     
     
     return 0;
