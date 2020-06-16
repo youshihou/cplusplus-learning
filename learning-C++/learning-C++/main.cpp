1c1a5fbb81491e2cba3436ea9468621d48a69f59
oneday <youshihou5@gmail.com>
2020-06-16 19:20:26 +0800

    anonymous object

--- learning-C++/learning-C++/main.cpp
+++ learning-C++/learning-C++/main.cpp
@@ -9,59 +9,12 @@
 #include <iostream>
 using namespace std;
 
-class Car {
-public:
-    Car() {
-        cout << __PRETTY_FUNCTION__ << " - " << this << endl;
-    }
-    Car(const Car &car) {
-        cout << __PRETTY_FUNCTION__ << " - " << this << endl;
-    }
-    ~Car() {
-        cout << __PRETTY_FUNCTION__ << " - " << this << endl;
-    }
-    void run() {
-        cout << __PRETTY_FUNCTION__ << endl;
-    }
-};
 
-void func(Car car) { // here will call copy constructor
-    
-}
-void func_(Car &car) {
-    car.run();
-}
-void func__(Car *car) {
-    car->run();
-}
-
-Car test() {
-    Car car;
-    return car;
-}
-
-Car test_() {
-    return Car();
-}
 
 
 int main(int argc, const char * argv[]) {
-//    Car car1;
-//    func(car1);
-//    func_(car1);
-//    func__(&car1);
+
     
-//    Car car2;
-//    car2 = test();
-    
-//    Car car3 = test();
-    
-    
-//    Car().run();
-//    func(Car());
-    
-    Car car;
-    car = test_();
     
     return 0;
 }
