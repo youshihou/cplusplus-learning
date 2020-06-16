74800043e4120c827e33770839b76a7e277188a7
oneday <youshihou5@gmail.com>
2020-06-16 22:36:40 +0800

    operator overload

--- learning-C++/learning-C++/main.cpp
+++ learning-C++/learning-C++/main.cpp
@@ -9,12 +9,40 @@
 #include <iostream>
 using namespace std;
 
+class Point {
+    friend Point add(Point, Point);
+//    friend Point operator+(Point, Point);
+    friend Point operator+(const Point &, const Point &);
+    int m_x;
+    int m_y;
+public:
+    Point(int x, int y) :m_x(x), m_y(y) {}
+    void display() {
+        cout << "(" << m_x << ", " << m_y << ")" << endl;
+    }
+};
 
+Point add(Point p1, Point p2) {
+    return Point(p1.m_x + p2.m_x, p1.m_y + p2.m_y);
+}
 
+//Point operator+(Point p1, Point p2) {
+//    return Point(p1.m_x + p2.m_x, p1.m_y + p2.m_y);
+//}
+
+Point operator+(const Point &p1, const Point &p2) {
+    return Point(p1.m_x + p2.m_x, p1.m_y + p2.m_y);
+}
 
 
 int main(int argc, const char * argv[]) {
-
+    Point p1(10, 20);
+    Point p2(20, 30);
+//    Point p = add(p1, p2);
+//    p.display();
+        
+    Point p3 = p1 + p2; // equivalent to Point p3 = operator+(p1, p2);
+    p3.display();
     
     getchar();
     return 0;
