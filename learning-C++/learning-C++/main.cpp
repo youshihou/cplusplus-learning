77fd5a367905058992f92abda6cfd5e620e8b6e5
oneday <youshihou5@gmail.com>
2020-06-16 19:55:08 +0800

    implicit and explicit constructor

--- learning-C++/learning-C++/main.cpp
+++ learning-C++/learning-C++/main.cpp
@@ -9,52 +9,11 @@
 #include <iostream>
 using namespace std;
 
-class Person {
-    int m_age;
-public:
-    Person() {
-        cout << __PRETTY_FUNCTION__ << " - " << this << endl;
-    }
-    Person(int age) :m_age(age) {
-        cout << __PRETTY_FUNCTION__ << " - " << this << endl;
-    }
-    // default allow implicit constructor
-    // explicit do not allow implicit constructor
-//    explicit Person(int age) :m_age(age) {
-//        cout << __PRETTY_FUNCTION__ << " - " << this << endl;
-//    }
-    Person(const Person &person) {
-        cout << __PRETTY_FUNCTION__ << " - " << this << endl;
-    }
-    ~Person() {
-        cout << __PRETTY_FUNCTION__ << " - " << this << endl;
-    }
-    void display() {
-        cout << __PRETTY_FUNCTION__ << " - " << this->m_age << endl;
-    }
-};
 
-void func(Person person) {
-    
-}
-
-Person func_() {
-    return 40; // call Person(int age)
-}
 
 int main(int argc, const char * argv[]) {
-//    Person p1;
-//    Person p2(10);
-//    Person p3 = p2;
+
     
-//    Person p4 = 20; // call Person(int age), equuivalent to Person p4(20);
-    
-//    func(30); // call Person(int age)
-    
-//    func_();
-    
-    Person p; // call Person()
-    p = 50; // call Person(50)
     
     
     getchar();
