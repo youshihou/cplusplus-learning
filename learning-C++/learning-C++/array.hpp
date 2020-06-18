f709c8b2ed79c86c361686b2e9e3272292f8ece7
oneday <youshihou5@gmail.com>
2020-06-18 23:25:44 +0800

    template

---
+++ learning-C++/learning-C++/array.hpp
@@ -0,0 +1,14 @@
+//
+//  array.hpp
+//  learning-C++
+//
+//  Created by Ankui on 6/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef array_hpp
+#define array_hpp
+
+#include <stdio.h>
+
+#endif /* array_hpp */
