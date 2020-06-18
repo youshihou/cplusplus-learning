ea770f51fd7ca5e43172228cf5bac121f90a2262
oneday <youshihou5@gmail.com>
2020-06-18 23:25:50 +0800

    template

--- learning-C++/learning-C++/array.hpp
+++ learning-C++/learning-C++/array.hpp
@@ -9,6 +9,101 @@
 #ifndef array_hpp
 #define array_hpp
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+template <class Element>
+class Array {
+    Element *m_data;
+    int m_size;
+    int m_capacity;
+    
+    void check_index(int index);
+    Element get(int index);
+public:
+    Array(int capacity = 0);
+    ~Array();
+    
+    int size();
+    void add(Element value);
+    void remove(int index);
+    Element operator[](int index);
+    friend ostream & operator<<(ostream &, const Array<Element> &);
+};
+
+template <class Element>
+void Array<Element>::check_index(int index) {
+    if (index < 0 || index >= m_size) {
+        throw "index error!";
+    }
+}
+
+template <class Element>
+Element Array<Element>::get(int index) {
+    check_index(index);
+    return m_data[index];
+}
+
+template <class Element>
+Array<Element>::Array(int capacity) { // CARE!!! default parameter only write in declaration
+    m_capacity = capacity > 0 ? capacity : 10;
+    m_data = new Element[capacity]();
+}
+
+template <class Element>
+Array<Element>::~Array() {
+    if (m_data == nullptr) { return; }
+    delete [] m_data;
+    m_data = nullptr;
+}
+
+template <class Element>
+int Array<Element>::size() {
+    return m_size;
+}
+
+template <class Element>
+void Array<Element>::add(Element value) {
+    if (m_size == m_capacity) {
+        Element *old_data = m_data;
+        size_t new_size = m_capacity << 1;
+        Element *new_data = new Element[new_size]();
+        memcpy(new_data, old_data, new_size);
+        m_data = new_data;
+        delete [] old_data;
+        
+        cout << "new_size = " << new_size << endl;
+    }
+    m_data[m_size++] = value;
+}
+
+template <class Element>
+void Array<Element>::remove(int index) {
+    check_index(index);
+    
+    
+}
+
+template <class Element>
+Element Array<Element>::operator[](int index) {
+    return get(index);
+}
+
+
+template <class Element>
+ostream & operator<<(ostream &cout, const Array<Element> &array) {
+    cout << "[";
+    for (int i = 0; i < array.m_size; i++) {
+        if (i != 0) {
+            cout << ", ";
+        }
+        cout << array.m_data[i];
+//        if (i != array.m_size - 1) {
+//            cout << ", ";
+//        }
+    }
+    return cout << "]";
+}
+
 
 #endif /* array_hpp */
--- learning-C++/learning-C++/main.cpp
+++ learning-C++/learning-C++/main.cpp
@@ -6,18 +6,163 @@
 //  Copyright © 2020 Ankui. All rights reserved.
 //
 
+#include "array.hpp"
 #include <iostream>
 using namespace std;
+
+#if 1
+class Point {
+    friend ostream & operator<<(ostream &, const Point &);
+    int m_x;
+    int m_y;
+public:
+    Point(int x = 0, int y = 0) :m_x(x), m_y(y) {}
+    const Point operator+(const Point &point) const {
+        return Point(m_x + point.m_x, m_y + point.m_y);
+    }
+};
+ostream & operator<<(ostream &cout, const Point & point) {
+    return cout << "(" << point.m_x << ", " << point.m_y << ")" << endl;
+//    return cout;
+}
+Point add(Point a, Point b) {
+    return a + b;
+}
+int add(int a, int b) {
+    return a + b;
+}
+double add(double a, double b) {
+    return a + b;
+}
+
+
+template <typename T> T add(T a, T b) { // in here typename == class
+    return a + b;
+}
+#endif
+
+
+#if 0
+class Array {
+    int *m_data;
+    int m_size;
+    int m_capacity;
+    
+    int get(int index) {
+        if (index < 0 || index >= m_size) {
+            throw "index error!";
+        }
+        return m_data[index];
+    }
+public:
+    Array(int capacity = 0) {
+        m_capacity = capacity > 0 ? capacity : 10;
+        m_data = new int[capacity]();
+    }
+    ~Array() {
+        if (m_data == nullptr) { return; }
+        delete [] m_data;
+        m_data = nullptr;
+    }
+    
+    int size() {
+        return m_size;
+    }
+    void add(int value) {
+        if (m_size == m_capacity) {
+            int *old_data = m_data;
+            size_t new_size = m_capacity << 1;
+            int *new_data = new int[new_size]();
+            memcpy(new_data, old_data, new_size);
+            m_data = new_data;
+            delete [] old_data;
+            
+            cout << "new_size = " << new_size << endl;
+        }
+        m_data[m_size++] = value;
+    }
+    int operator[](int index) {
+        return get(index);
+    }
+};
+#endif
+
+
+#if 0
+template <class Element>
+class Array {
+    Element *m_data;
+    int m_size;
+    int m_capacity;
+    
+    Element get(int index) {
+        if (index < 0 || index >= m_size) {
+            throw "index error!";
+        }
+        return m_data[index];
+    }
+public:
+    Array(int capacity = 0) {
+        m_capacity = capacity > 0 ? capacity : 10;
+        m_data = new Element[capacity]();
+    }
+    ~Array() {
+        if (m_data == nullptr) { return; }
+        delete [] m_data;
+        m_data = nullptr;
+    }
+    
+    int size() {
+        return m_size;
+    }
+    void add(Element value) {
+        if (m_size == m_capacity) {
+            int *old_data = m_data;
+            size_t new_size = m_capacity << 1;
+            int *new_data = new Element[new_size]();
+            memcpy(new_data, old_data, new_size);
+            m_data = new_data;
+            delete [] old_data;
+            
+            cout << "new_size = " << new_size << endl;
+        }
+        m_data[m_size++] = value;
+    }
+    Element operator[](int index) {
+        return get(index);
+    }
+};
+#endif
+
 
 
 
 
 int main(int argc, const char * argv[]) {
 
+//    cout << add(10, 20) << endl;
+//    cout << add(1.0, 2.0) << endl;
+//    cout << add(Point(1, 2), Point(3, 4)) << endl;
+//    add<int>(10, 20);
+//    add<double>(1.1, 2.2);
+//    add<Point>(Point(1, 1), Point(2, 2));
     
     
+    Array<int> array;
+    for (int i = 1; i <= 11; i++) {
+        array.add(i * 10);
+    }
+//    cout << array << endl;
+
+//    cout << "size = " << array.size() << endl;
+//    cout << array.get(0) << endl;
+//    cout << array[0] << endl;
+//    cout << array[11] << endl;
     
-    
+//    Array<Point> array;
+//    array.add(Point(1, 2));
+//    array.add(Point(3, 4));
+//    cout << array << endl;
     
     return 0;
 }
