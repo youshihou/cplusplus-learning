cd6af33dbf79e44f838b12e7d27bbe6f3a78302f
oneday <youshihou5@gmail.com>
2020-06-20 11:44:59 +0800

    exception

--- learning-C++/learning-C++/main.cpp
+++ learning-C++/learning-C++/main.cpp
@@ -9,15 +9,51 @@
 #include <iostream>
 using namespace std;
 
+class Exception {
+public:
+    virtual const char * what() const = 0;
+    virtual int code() const = 0;
+};
+class DividdException: public Exception {
+public:
+    const char * what() const override {
+        return "must be not zero.";
+    }
+    int code() const override {
+        return 404;
+    }
+};
+
+
+int divide(int v1, int v2) {
+    if (v2 == 0) {
+        throw DividdException();
+    }
+    return v1 / v2;
+}
 
 
 
 int main(int argc, const char * argv[]) {
+//    try {
+//        int a = 10;
+//        int b = 0;
+//        divide(a, b);
+//    } catch (const DividdException &e) {
+//        cout << "capture exception - " << e.code() << ", " << e.what() << endl;
+//    }
+    
+    
+    try {
+        int a = 10;
+        int b = 0;
+        divide(a, b);
+    } catch (const Exception &e) {
+        cout << "capture exception - " << e.code() << ", " << e.what() << endl;
+    }
 
     
-    
-    
-    
+    bad_alloc e;
     
     
     return 0;
